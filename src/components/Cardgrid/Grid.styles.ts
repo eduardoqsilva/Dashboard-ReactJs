954d55e72808b38e4f534db4679799e690b90b4c
eduardoqsilva <eduadsilva@outlook.com>
2023-01-31 16:11:05 -0300

    add grid component

---
+++ src/components/Cardgrid/Grid.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const GridWrapper = styled.main`
+
+  padding: 1rem;
+
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  gap: 1rem;
+`
